ec6ccb348d71addeb42fee69460c6ad9b25054f5
hamid reza zare pour <hamidreza.num1@gmail.com>
2020-01-27 11:54:28 +0330

    New version

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 
 setup(name='aljson',
-      version='0.2.0',
+      version='0.3.0',
       description='Convert a SqlAlchemy query object to a dict(json)',
       long_description=open('README.rst').read(),
       url='https://github.com/hrzp/aljson',
-      download_url='https://github.com/hrzp/aljson/archive/0.2.0.tar.gz',
+      download_url='https://github.com/hrzp/aljson/archive/0.3.0.tar.gz',
       author='Hamid Reza Zarepour',
       author_email='hamid.zarepour.dev@gmail.com',
       license='MIT',
