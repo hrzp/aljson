c903e6a50f2d9ff9d928b52a552628b1a038ee0a
hamid reza zare pour <hamidreza.num1@gmail.com>
2020-01-27 10:41:28 +0330

    Add required packages

--- setup.py
+++ setup.py
@@ -7,8 +7,11 @@ setup(name='aljson',
       description='Convert a SqlAlchemy query object to a dict',
       long_description=open('README.rst').read(),
       url='https://github.com/hrzp/aljson',
+      download_url='https://github.com/hrzp/aljson/archive/0.1.0.tar.gz',
       author='Hamid Reza Zarepour',
       author_email='hamid.zarepour.dev@gmail.com',
       license='MIT',
       packages=['aljson'],
-      zip_safe=False)
+      zip_safe=False,
+      install_requires=['sqlalchemy']
+      )
